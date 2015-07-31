fb533fb2c248709102fe83df353a2a954be1aee3
Cristian Rodriguez <cfrodca@gmail.com>
2015-07-31 18:41:17 -0500

    Create auth.h

---
+++ ssh/auth.h
@@ -0,0 +1,13 @@
+/*
+ * auth.h
+ *
+ *  Created on: 24/07/2015
+ *      Author: Cristian
+ */
+
+#ifndef SERVERS_SSH_AUTH_H_
+#define SERVERS_SSH_AUTH_H_
+
+int auth_hd(SSH *);
+
+#endif /* SERVERS_SSH_AUTH_H_ */
